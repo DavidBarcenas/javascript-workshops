4769b7de85ad894b3bf2a353f5765f7d3e111bff
David Barcenas <davidbarcenasmx@gmail.com>
2022-08-21 22:28:28 -0500

    create general poster

---
+++ movies/src/poster.js
@@ -0,0 +1,60 @@
+import { $, createElement, getImage, getRating, getYear } from './helpers';
+
+function Poster(movies, section, useBackdrop = false) {
+  movies.forEach((movie) => {
+    const image = useBackdrop ? movie.backdrop_path : movie.poster;
+    const movieContainer = createElement('figure');
+    movieContainer.classList.add('img-container');
+
+    const movieImg = createElement('img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute('src', getImage(image));
+
+    const movieDetails = createElement('figcaption');
+    const movieDetailsContainer = createElement('div');
+    movieDetailsContainer.classList.add('img-description');
+
+    const movieTitle = createElement('h3');
+    movieTitle.textContent = movie.title;
+
+    const movieRelease = createElement('span');
+    movieRelease.classList.add('year', 'light-text');
+    movieRelease.textContent = getYear(movie.release_date);
+
+    const movieRating = createElement('span');
+    movieRating.classList.add('rating');
+    movieRating.textContent = getRating(movie.vote_average) + ' rating';
+
+    const movieActions = createElement('div');
+    movieActions.classList.add('img-actions');
+
+    const movieWatchBtn = createElement('button');
+    movieWatchBtn.classList.add('btn-primary');
+    movieWatchBtn.textContent = 'Watch now';
+
+    const movieAddBtn = createElement('button');
+    movieAddBtn.classList.add('btn-plus');
+
+    const movieAddBtnIcon = createElement('i');
+    movieAddBtnIcon.classList.add('fa-solid', 'fa-plus');
+
+    movieAddBtn.appendChild(movieAddBtnIcon);
+
+    movieDetailsContainer.appendChild(movieTitle);
+    movieDetailsContainer.appendChild(movieRelease);
+    movieDetailsContainer.appendChild(movieRating);
+
+    movieActions.appendChild(movieWatchBtn);
+    movieActions.appendChild(movieAddBtn);
+
+    movieDetails.appendChild(movieDetailsContainer);
+    movieDetails.appendChild(movieActions);
+
+    movieContainer.appendChild(movieImg);
+    movieContainer.appendChild(movieDetails);
+
+    $(section).appendChild(movieContainer);
+  });
+}
+
+export default Poster;
